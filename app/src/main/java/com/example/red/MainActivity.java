2f624e219223fba21aef7288ea577148036b7093
Ritik Mathur <1824433@brunel.ac.uk>
2020-03-02 11:19:27 +0000

    Navigation Page -v2 Author: Ritik Mathur (1824433; @rmathur00 ) Co-Author: Ashish Menghani (1836076; @AshishMenghani27 )
    Description:
    1) Added Icon
    2) Fragmentation to the bar

--- app/src/main/java/com/example/red/MainActivity.java
+++ app/src/main/java/com/example/red/MainActivity.java
@@ -1,14 +1,95 @@
 package com.example.red;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.Toolbar;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView mMainNav;
+    private FrameLayout mMainFrame;
+
+    private Beam_fragment beam_fragment;
+    private Booking_fragment booking_fragment;
+    private Chat_fragment chat_fragment;
+    private Feed_fragment feed_fragment;
+    private Profile_fragment profile_fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMainFrame = findViewById(R.id.main_Frame);
+        mMainNav = findViewById(R.id.main_nav);
+
+        // Initialising Fragments
+        beam_fragment = new Beam_fragment();
+        booking_fragment = new Booking_fragment();
+        chat_fragment = new Chat_fragment();
+        feed_fragment = new Feed_fragment();
+        profile_fragment = new Profile_fragment();
+
+        setFragment(beam_fragment);
+
+        // Code for App Icon (Action Bar)
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setIcon(R.mipmap.app_icon_round);
+
+        //Listening the clicks on Navigation Bottom Bar
+        mMainNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+
+                    case R.id.nav_beam:{
+                        setFragment(beam_fragment);
+                        return true;
+                    }
+                    case R.id.nav_book:{
+                        setFragment(booking_fragment);
+                        return true;
+                    }
+                    case R.id.nav_chat:{
+                        setFragment(chat_fragment);
+                        return true;
+                    }
+                    case R.id.nav_feed:{
+                        setFragment(feed_fragment);
+                        return true;
+                    }
+                    case R.id.nav_profile:{
+                        setFragment(profile_fragment);
+                        return true;
+                    }
+                    default:
+                        return false;
+
+                }
+            }
+        });
+
     }
+
+    private void setFragment(Fragment fragment) {
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_Frame, fragment);
+        fragmentTransaction.commit();
+
+    }
+
+
 }
