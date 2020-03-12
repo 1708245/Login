ba45dfaf718588bc781935681406faa1b6156ae5
Ritik Mathur <1824433@brunel.ac.uk>
2020-03-12 15:37:06 +0000

    New Changes 1) Main page functions changed to Hope page

---
+++ app/src/main/java/com/example/red/Home_page.java
@@ -0,0 +1,83 @@
+package com.example.red;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Home_page extends AppCompatActivity {
+    private BottomNavigationView mMainNav;
+    private FrameLayout mMainFrame;
+
+    private Beam_fragment beam_fragment;
+    private Booking_fragment booking_fragment;
+    private Chat_fragment chat_fragment;
+    private Feed_fragment feed_fragment;
+    private Profile_fragment profile_fragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+        mMainFrame = findViewById(R.id.main_Frame);
+        mMainNav = findViewById(R.id.main_nav);
+
+        // Initialising Fragments
+        beam_fragment = new Beam_fragment();
+        booking_fragment = new Booking_fragment();
+        chat_fragment = new Chat_fragment();
+        feed_fragment = new Feed_fragment();
+        profile_fragment = new Profile_fragment();
+
+        setFragment(booking_fragment);
+
+        /*
+        Listening the clicks on Navigation Bottom Bar
+         */
+        mMainNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+
+                    case R.id.nav_beam: {
+                        setFragment(beam_fragment);
+                        return true;
+                    }
+                    case R.id.nav_book: {
+                        setFragment(booking_fragment);
+                        return true;
+                    }
+                    case R.id.nav_chat: {
+                        setFragment(chat_fragment);
+                        return true;
+                    }
+                    case R.id.nav_feed: {
+                        setFragment(feed_fragment);
+                        return true;
+                    }
+                    case R.id.nav_profile: {
+                        setFragment(profile_fragment);
+                        return true;
+                    }
+                    default:
+                        return false;
+
+                }
+            }
+        });
+
+    }
+    private void setFragment(Fragment fragment) {
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_Frame, fragment);
+        fragmentTransaction.commit();
+
+    }
+}
--- app/src/main/java/com/example/red/MainActivity.java
+++ app/src/main/java/com/example/red/MainActivity.java
@@ -1,89 +1,35 @@
 package com.example.red;
 
-import androidx.annotation.NonNull;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
+
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.FrameLayout;
+import android.view.View;
+import android.widget.Button;
 
-
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private BottomNavigationView mMainNav;
-    private FrameLayout mMainFrame;
-
-    private Beam_fragment beam_fragment;
-    private Booking_fragment booking_fragment;
-    private Chat_fragment chat_fragment;
-    private Feed_fragment feed_fragment;
-    private Profile_fragment profile_fragment;
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    button = findViewById(R.id.button);
+    button.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            onclick(view);
+        }
+    });
 
-        mMainFrame = findViewById(R.id.main_Frame);
-        mMainNav = findViewById(R.id.main_nav);
-
-        // Initialising Fragments
-        beam_fragment = new Beam_fragment();
-        booking_fragment = new Booking_fragment();
-        chat_fragment = new Chat_fragment();
-        feed_fragment = new Feed_fragment();
-        profile_fragment = new Profile_fragment();
-
-        setFragment(booking_fragment);
-
-        /*
-        Listening the clicks on Navigation Bottom Bar
-         */
-        mMainNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch (menuItem.getItemId()) {
-
-                    case R.id.nav_beam: {
-                        setFragment(beam_fragment);
-                        return true;
-                    }
-                    case R.id.nav_book: {
-                        setFragment(booking_fragment);
-                        return true;
-                    }
-                    case R.id.nav_chat: {
-                        setFragment(chat_fragment);
-                        return true;
-                    }
-                    case R.id.nav_feed: {
-                        setFragment(feed_fragment);
-                        return true;
-                    }
-                    case R.id.nav_profile: {
-                        setFragment(profile_fragment);
-                        return true;
-                    }
-                    default:
-                        return false;
-
-                }
-            }
-        });
 
     }
-
-    private void setFragment(Fragment fragment) {
-
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.main_Frame, fragment);
-        fragmentTransaction.commit();
-
+    private void onclick(View view){
+        Intent intent = new Intent(this, Home_page.class);
+        startActivity(intent);
     }
 
 
