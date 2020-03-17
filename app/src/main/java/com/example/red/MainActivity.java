0548ae4fc04c8fcd1149964a112075343df4b0d3
1708245 <1708245@brunel.ac.uk>
2020-03-17 22:30:57 +0000

    Login

--- app/src/main/java/com/example/red/MainActivity.java
+++ app/src/main/java/com/example/red/MainActivity.java
@@ -1,36 +1,188 @@
 package com.example.red;
 
 
-import androidx.appcompat.app.AppCompatActivity;
-
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 
 public class MainActivity extends AppCompatActivity {
+    public static final int Google_SIgnIn_Code = 10005;
+    SignInButton google;
+    GoogleSignInOptions gso;
+    GoogleSignInClient signInClient;
+    EditText mEmail, mPassword;
+    Button mLoginBtn, morganisationBtn,mdonor;
+    TextView mButton2;
+    TextView mCreateBtn;
+    ProgressBar progressBar;
+    FirebaseAuth fAuth;
+
 
     private Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    button = findViewById(R.id.button);
-    button.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            onclick(view);
+
+        google = findViewById(R.id.google);
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+               .requestIdToken("541692718714-a4od4uo4gi94h8ch0iuiinc1g3tk878a.apps.googleusercontent.com")
+                .requestEmail()
+                .requestProfile()
+                .requestId()
+                .build();
+        signInClient = GoogleSignIn.getClient(this, gso);
+        GoogleSignInAccount signInAccount = GoogleSignIn.getLastSignedInAccount(this);
+        if (signInAccount != null) {
+            startActivity(new Intent(this,Home_page.class));
+           // Toast.makeText(this, "User is ALready Logged in", Toast.LENGTH_SHORT).show();
         }
-    });
+
+        google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sign = signInClient.getSignInIntent();
+                startActivityForResult(sign,
+                        Google_SIgnIn_Code);
+            }
+        });
+
+
+        mEmail = findViewById(R.id.Email);
+        mPassword = findViewById(R.id.Password);
+        progressBar = findViewById(R.id.progressBar2);
+        fAuth = FirebaseAuth.getInstance();
+        morganisationBtn = findViewById(R.id.Organisation);
+        mLoginBtn = findViewById(R.id.Loginbutton);
+        mButton2 = findViewById(R.id.button2);
+        mdonor=findViewById(R.id.button);
+        // mButton2 = findViewById(R.id.button2);
+        mCreateBtn = findViewById(R.id.Signup);
+
+        morganisationBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // startActivity(new Intent(getApplicationContext(), Register_page.class));
+                onclickmethod();
+            }
+        });
+
+        mdonor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // startActivity(new Intent(getApplicationContext(), Register_page.class));
+                onclickmethod1();
+            }
+        });
+
+        //mButton2.setOnClickListener(new View.OnClickListener() {
+        // @Override
+        //public void onClick(View view) {
+        //startActivity(new Intent(getApplicationContext(), Register_donor_page.class));
+        // onclickmethod();
+        //}
+        //});
+
+
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    mEmail.setError("Email is Required.");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    mPassword.setError("Password is Required.");
+                    return;
+                }
+
+                if (password.length() < 6) {
+                    mPassword.setError("Password Must be >= 6 Characters");
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                // authenticate the user
+
+                fAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), Home_page.class));
+                        } else {
+                            Toast.makeText(MainActivity.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+
+                    }
+                });
+
+
+            }
+        });
 
 
     }
-    private void onclick(View view){
-        Intent intent = new Intent(this, Home_page.class);
+
+    private void onclickmethod1() {
+        Intent intent = new Intent(this, Register_donor_page.class);
         startActivity(intent);
     }
 
+    public void onclickmethod() {
+        Intent intent = new Intent(this, Register_page.class);
+        startActivity(intent);
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == Google_SIgnIn_Code) {
+            Task<GoogleSignInAccount> signInTask = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try {
+                GoogleSignInAccount signInAcc = signInTask.getResult(ApiException.class);
+                AuthCredential authCredential = GoogleAuthProvider.getCredential(signInAcc.getIdToken(),null);
+                startActivity(new Intent(this,Home_page.class));
+                Toast.makeText(this,"Your Google Account is Connected to our Application",Toast.LENGTH_SHORT).show();
+            } catch (ApiException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
 }
---
+++ app/src/main/java/com/example/red/Register_donor_page.java
@@ -0,0 +1,150 @@
+package com.example.red;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Register_donor_page extends AppCompatActivity {
+
+    public static final String TAG = "TAG";
+
+    EditText mFullName,mEmail,mPassword,mdateofbirth,maddress,maddress2,mcounty,mpost,mblood,mweight,mtrans,mcancer;
+    Button mRegisterBtn;
+    TextView mLogin1Btn;
+    FirebaseAuth fAuth;
+    ProgressBar progressBar;
+    FirebaseFirestore fstore;
+    String userID;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_donor_page);
+
+        mFullName = findViewById(R.id.FullNAme1);
+        mEmail = findViewById(R.id.Email1);
+        mPassword = findViewById(R.id.Password1);
+        mRegisterBtn = findViewById(R.id.Register1);
+        mLogin1Btn = findViewById(R.id.Login1);
+        maddress = findViewById(R.id.Address1);
+        maddress2 = findViewById(R.id.address21);
+        mcounty = findViewById(R.id.County1);
+        mpost = findViewById(R.id.Postcode1);
+        fAuth = FirebaseAuth.getInstance();
+        fstore = FirebaseFirestore.getInstance();
+
+
+        if (fAuth.getCurrentUser() != null) {
+            startActivity(new Intent(getApplicationContext(), Home_page.class));
+            finish();
+        }
+        mLogin1Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
+
+        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+                final String FullName1 = mFullName.getText().toString();
+                final String address = maddress.getText().toString();
+                final String address2 = maddress2.getText().toString();
+                final String Countyy = mcounty.getText().toString();
+                final String Postcode = mpost.getText().toString();
+
+                if (TextUtils.isEmpty(email)) {
+                    mEmail.setError("Email is Required.");
+                    return;
+
+                }
+                if (TextUtils.isEmpty(password)) {
+                    mPassword.setError("Password is Required.");
+                    return;
+
+                }
+                if (password.length() < 6) {
+                    mPassword.setError("Password Must be >= 6 Characters");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(address)) {
+                    maddress.setError("address is Required.");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(Countyy)) {
+                    mcounty.setError("County is Required.");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(Postcode)) {
+                    mpost.setError("Postcode is Required.");
+                    return;
+                }
+
+
+                fAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(Register_donor_page.this, "User Created", Toast.LENGTH_SHORT).show();
+                            userID = fAuth.getCurrentUser().getUid();
+                            DocumentReference documentreference = fstore.collection("users").document(userID);
+                            Map<String, Object> user = new HashMap<>();
+                            user.put("fName", FullName1);
+                            user.put("email", email);
+                            user.put("address", address);
+                            user.put("address2", address2);
+                            user.put("County", Countyy);
+                            user.put("Postcode", Postcode);
+                            documentreference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d(TAG, "onSuccess: user Profile is created for " + userID);
+                                }
+                            });
+                            startActivity(new Intent(getApplicationContext(), Home_page.class));
+
+                        } else {
+                            Toast.makeText(Register_donor_page.this, "Error" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+
+
+                        mLogin1Btn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    }
+                });
+            }
+        })
+        ;
+    }
+});}
+}
